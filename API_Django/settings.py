2101ab5a427b1ed5420a34b3a6ad1bfca8abb67a
Viktor Stoyanov <victor.stoyanov@edu.itescia.fr>
2021-10-08 23:36:47 +0200

    some fix

--- API_Django/settings.py
+++ API_Django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 env = environ.Env()
 environ.Env.read_env()
@@ -189,3 +190,5 @@ AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE= False
 #AWS_DEFAULT_ACL= None
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
