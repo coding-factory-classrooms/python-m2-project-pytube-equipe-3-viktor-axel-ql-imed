ef8ca473ce5417c7683b25496b1f9286dfe6de29
Viktor Stoyanov <victor.stoyanov@edu.itescia.fr>
2021-10-08 19:16:06 +0200

    videos successfully displaying inside browser, feature done can merge

--- API_Django/API_Django/settings.py
+++ API_Django/API_Django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'API_Django')
+
+print('MEDIA_ROOT', MEDIA_ROOT)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
