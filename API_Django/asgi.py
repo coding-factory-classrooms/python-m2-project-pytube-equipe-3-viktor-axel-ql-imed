55f1fc40981bf40a137c759a6de25afdeb0b1a06
QuentinL <quentinloicp@gmail.com>
2021-10-11 17:44:16 +0200

    Début story 8

--- API_Django/asgi.py
+++ API_Django/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+import video.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API_Django.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            video.routing.websocket_urlpatterns
+        )
+    ),
+    "http": get_asgi_application(),
+})
